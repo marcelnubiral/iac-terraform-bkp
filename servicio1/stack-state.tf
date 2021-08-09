c3f6bc2af3252a6a14e0bcc72a31ed8f7d69c0ee
nicolas@nubiral.com <nicolas@nubiral.com>
2021-08-09 13:12:06 -0300

    update

--- servicio1/stack-state.tf
+++ servicio1/stack-state.tf
@@ -2,9 +2,10 @@ terraform {
   backend "s3"  {
     encrypt = "true"
     bucket = "repoadsbx2"
-    dynamodb_table = "lisandro-arcos-terraform-tfstate"
+    dynamodb_table = "iac-nubiral-terraform-tfstate"
     region = "us-east-1"
     key = "lnx/lnxservice.tfstate"
+    role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_role_name}"
   }
   required_providers {
     awx = {
