9517fe64686738ee47df660ad75a079e35cb44bb
nicolas@nubiral.com <nicolas@nubiral.com>
2021-08-09 13:21:35 -0300

    update

--- servicio1/ec2.tf
+++ servicio1/ec2.tf
@@ -39,10 +39,10 @@ resource "aws_instance" "srv" {
 
   root_block_device = {
     delete_on_termination = true
-    encrtypted = true
-    kms_key_id = var.ec2_root_kms_id
-    volume_size = 20
-    volume_type = gp2
+    encrtypted            = true
+    kms_key_id            = var.ec2_root_kms_id
+    volume_size           = 20
+    volume_type           = gp2
   }
 
   tags = {
