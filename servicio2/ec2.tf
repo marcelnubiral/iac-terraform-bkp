36c3cf616765512eaef615f89b9ebb7672ac6994
nicolas@nubiral.com <nicolas@nubiral.com>
2021-10-20 13:58:02 -0300

    update

--- servicio2/ec2.tf
+++ servicio2/ec2.tf
@@ -113,4 +113,5 @@ resource "awx_host" "axwnode" {
   ]
   enabled   = true
   variables = "ansible_host: ${element(aws_instance.srv.*.private_ip, count.index)}"
-}                                                                                
+}
+#                                                                                
