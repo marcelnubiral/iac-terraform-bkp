b50383bb9a3bde2a09089ee9ab96148208ef3cae
nicolas <nicolas@nubiral.com>
2022-01-04 14:58:35 -0300

    update

--- servicio2/ec2.tf
+++ servicio2/ec2.tf
@@ -100,7 +100,7 @@ resource "aws_instance" "srv" {
     shutdownschedule          = "8a20"
     productowneremail         = "Gonzalo.Aresrivas@ar.mcd.com"
   }
-}
+}#
 
 # resource "awx_host" "axwnode" {
 #   count = local.instances_count
