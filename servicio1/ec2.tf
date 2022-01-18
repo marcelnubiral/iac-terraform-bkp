b632d6c6394085a8f2b734ae6d5a81c77d453112
nicolas <nicolas@nubiral.com>
2022-01-18 13:40:46 -0300

    update

--- servicio1/ec2.tf
+++ servicio1/ec2.tf
@@ -57,7 +57,7 @@ data "aws_iam_instance_profile" "s3-access-role" {
 resource "aws_instance" "srv" {
   count                       = local.instances_count
   #ami                         = "${data.aws_ami.oracle.id}"
-  ami                         = "ami-076154aee70227f83"
+  ami                         = "ami-0ab6bf43b612c96b9"
   key_name                    = var.ec2_key_name
   iam_instance_profile        = data.aws_iam_instance_profile.s3-access-role.name
   vpc_security_group_ids      = var.ec2_security_groups
