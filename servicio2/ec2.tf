87ccf24cd28689697cecda112272f9ab62f5657c
nicolas <nicolas@nubiral.com>
2022-02-01 12:22:09 -0300

    update

--- servicio2/ec2.tf
+++ servicio2/ec2.tf
@@ -5,34 +5,34 @@ provider "aws" {
   }  
 }
 
-provider "awx" {
-  hostname = var.awx_host
-  insecure = var.awx_insecure
-  username = var.awx_user
-  password = var.awx_pass
-}
+# provider "awx" {
+#   hostname = var.awx_host
+#   insecure = var.awx_insecure
+#   username = var.awx_user
+#   password = var.awx_pass
+# }
 
-data "awx_organization" "default" {
-  name = var.awx_organization_name
-}
+# data "awx_organization" "default" {
+#   name = var.awx_organization_name
+# }
 
-data "awx_inventory" "default" {
-  name = var.awx_inventory_name
-}
+# data "awx_inventory" "default" {
+#   name = var.awx_inventory_name
+# }
 
-resource "awx_inventory_group" "default" {
-    name            = var.awx_inventory_group_name
-    inventory_id    = data.awx_inventory.default.id
-    variables       = <<YAML
-    ---
-    ansible_user: '${var.INSTANCE_USERNAME}'
-    ansible_password: '${var.INSTANCE_PASSWORD}'
-    ansible_connection: 'winrm'
-    ansible_winrm_server_cert_validation: 'ignore'
-    ansible_winrm_transport: 'basic'
-    ansible_winrm_scheme: 'https'
-YAML
-}
+# resource "awx_inventory_group" "default" {
+#     name            = var.awx_inventory_group_name
+#     inventory_id    = data.awx_inventory.default.id
+#     variables       = <<YAML
+#     ---
+#     ansible_user: '${var.INSTANCE_USERNAME}'
+#     ansible_password: '${var.INSTANCE_PASSWORD}'
+#     ansible_connection: 'winrm'
+#     ansible_winrm_server_cert_validation: 'ignore'
+#     ansible_winrm_transport: 'basic'
+#     ansible_winrm_scheme: 'https'
+# YAML
+# }
 
 locals {
   instances_count = 1
@@ -84,7 +84,7 @@ data "aws_ami" "windows"{
   }
 }
 
-resource "aws_instance" "srv" {
+resource "aws_instance" "srv1" {
   count                       = local.instances_count
   ami                         = "${data.aws_ami.windows.id}"
   #ami                         = "ami-0f540030bb04d884a"
@@ -111,17 +111,44 @@ resource "aws_instance" "srv" {
   }
 }
 
-resource "awx_host" "axwnode" {
-  count = local.instances_count
-  name         = "NUB-${var.aws_so}${count.index}${var.aws_n}-${var.aws_env}"
-  description  = "Nodo agregado desde terraform"
-  inventory_id = data.awx_inventory.default.id
-  group_ids = [ 
-    awx_inventory_group.default.id
-  ]
-  enabled   = true
-  variables = "ansible_host: ${element(aws_instance.srv.*.private_ip, count.index)}"
+resource "aws_instance" "srv2" {
+  count                       = local.instances_count
+  ami                         = "${data.aws_ami.windows.id}"
+  #ami                         = "ami-0f540030bb04d884a"
+  key_name                    = var.ec2_key_name
+  iam_instance_profile        = data.aws_iam_instance_profile.s3-access-role.name
+  vpc_security_group_ids      = var.ec2_security_groups
+  associate_public_ip_address = true
+  source_dest_check           = false
+  instance_type               = var.ec2_instance_type
+  subnet_id                   = var.ec2_subnet_id
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = true
+    kms_key_id            = var.ec2_root_kms_id
+    volume_size           = var.ec2_root_volume_size
+    volume_type           = var.ec2_root_volume_type
+  }
+  tags = {
+    Name                      = "NUB-${var.aws_so}${count.index}${var.aws_n}-${var.aws_env}"
+    productname               = "iac-nubiral"
+    environment               = var.aws_env
+    shutdownschedule          = "8a20"
+    productowneremail         = "Gonzalo.Aresrivas@ar.mcd.com"
+  }
 }
+
+# resource "awx_host" "axwnode" {
+#   count = local.instances_count
+#   name         = "NUB-${var.aws_so}${count.index}${var.aws_n}-${var.aws_env}"
+#   description  = "Nodo agregado desde terraform"
+#   inventory_id = data.awx_inventory.default.id
+#   group_ids = [ 
+#     awx_inventory_group.default.id
+#   ]
+#   enabled   = true
+#   variables = "ansible_host: ${element(aws_instance.srv.*.private_ip, count.index)}"
+# }
 
 
 
