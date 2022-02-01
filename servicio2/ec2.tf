03d3325369f68277d697ac9a21f0a8502d2eafd3
nicolas <nicolas@nubiral.com>
2022-02-01 11:22:04 -0300

    update

--- servicio2/ec2.tf
+++ servicio2/ec2.tf
@@ -86,8 +86,8 @@ data "aws_ami" "windows"{
 
 resource "aws_instance" "srv" {
   count                       = local.instances_count
-  #ami                         = "${data.aws_ami.windows.id}"
-  ami                         = "ami-0f540030bb04d884a"
+  ami                         = "${data.aws_ami.windows.id}"
+  #ami                         = "ami-0f540030bb04d884a"
   key_name                    = var.ec2_key_name
   iam_instance_profile        = data.aws_iam_instance_profile.s3-access-role.name
   vpc_security_group_ids      = var.ec2_security_groups
