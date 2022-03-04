c688c715db08231e14e798221c5b53fc90cebe84
Marcel <marcel@nubiral.com>
2022-03-04 12:48:25 -0300

    ebs

--- bi-uy-dev-win/ec2.tf
+++ bi-uy-dev-win/ec2.tf
@@ -110,11 +110,11 @@ resource "awx_host" "axwnode" {
 }
 
 resource "aws_ebs_volume" "web" {
-  // availability_zone = var.availability_zone
+  availability_zone = var.availability_zone
   size              = 44
   type = "gp3"
   encrypted =   true
   tags = {
-    Name = "nubiral"
+    Name = "nubiraltest"
   }
 }
