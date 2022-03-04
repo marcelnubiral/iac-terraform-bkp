188b3d56f680e24c3dc9c9b6c9c35231633c7ba5
Marcel <marcel@nubiral.com>
2022-03-04 16:46:16 -0300

    ebs

--- bi-uy-dev-win/ec2.tf
+++ bi-uy-dev-win/ec2.tf
@@ -122,6 +122,6 @@ resource "aws_ebs_volume" "data-vol" {
 resource "aws_volume_attachment" "srv-vol" {
   device_name = "/dev/sdc"
   volume_id   = "$(aws_ebs_volume.data-vol.id)"
-  instance_id = "${element(aws_instance.srv.*, count.index)}"
+  instance_id = "NUB-${var.aws_so}${count.index}${var.aws_n}-${var.aws_env}"
 }
 
