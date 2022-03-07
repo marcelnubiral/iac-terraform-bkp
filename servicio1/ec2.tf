0260292364d5ce611a5ffa230dd861a0a3cfedf6
nicolas <nicolas@nubiral.com>
2022-03-07 15:06:05 -0300

    update

--- servicio1/ec2.tf
+++ servicio1/ec2.tf
@@ -66,7 +66,7 @@ resource "aws_instance" "srv" {
   subnet_id                   = var.ec2_subnet_id
   user_data = <<EOF
     #!/bin/bash
-    mkdir test
+    mkdir /home/test
     echo ${var.domain_pwd} | realm join -U ${var.domain_user} aws.local
   EOF
   root_block_device {
