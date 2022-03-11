f70a8764de1faa66c3df1ab296f248ccbad0c7e0
nicolas <nicolas@nubiral.com>
2022-03-11 11:11:59 -0300

    update

--- servicio1/ec2.tf
+++ servicio1/ec2.tf
@@ -66,8 +66,7 @@ resource "aws_instance" "srv" {
   subnet_id                   = var.ec2_subnet_id
   user_data = <<EOF
   #! /bin/bash
-  sudo mkdir /home/prueba
-  sudo echo ${var.domain_pwd} | realm join -U ${var.domain_user} aws.local
+  sudo yum update -y
   EOF
   root_block_device {
     delete_on_termination = true
