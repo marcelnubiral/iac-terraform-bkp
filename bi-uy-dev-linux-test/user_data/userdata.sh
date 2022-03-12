407b3d15312b28e4ac918f4e9d49803f5343873a
Marcel <marcel@nubiral.com>
2022-03-11 23:35:42 -0300

    curll

--- bi-uy-dev-linux-test/user_data/userdata.sh
+++ bi-uy-dev-linux-test/user_data/userdata.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -x
-set -e
-
-sudo apt update
-sudo apt install -y nginx
-
+curl  www.google.com
 # cat << EOF > /home/ubuntu/test.txt
 # // ARCHIVO DE PRUEBA
 #    Contenido del archivo de texto.
