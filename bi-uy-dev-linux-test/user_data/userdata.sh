3f199801e177fe24893f25712b0fcc3819780219
Marcel <marcel@nubiral.com>
2022-03-23 22:13:03 -0300

    testing

--- bi-uy-dev-linux-test/user_data/userdata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo mkdir /home/pruebam
-sudo echo ${var.domain_pwd} | realm join -U ${var.domain_user} aws.local
--- dev/bi-uy-dev-linux-test/user_data/userdata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo mkdir /home/prueba
-sudo echo ${var.domain_pwd} | realm join -U ${var.domain_user} aws.local
