223452af6f61038dd633233891028c91d3c1b61a
Marcel <marcel@nubiral.com>
2022-03-26 12:15:12 -0300

    d

--- exa-cr-testing/stack-state.tf
+++ exa-cr-testing/stack-state.tf
@@ -4,8 +4,8 @@ terraform {
     bucket = "repoadsbx2"
     dynamodb_table = "iac-nubiral-terraform-tfstate"
     region = "us-east-1"
-    key = "lnx/lnxservice.tfstate"
-    #role_arn = "arn:aws:iam::884913712919:role/IAM-ROL-IAC-JNK"
+    key = "lnx/terraform.tfstate"
+    role_arn = "arn:aws:iam::884913712919:role/IAM-ROL-IAC-JNK"
   }
   required_providers {
     awx = {
