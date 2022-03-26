4726285a91b9bf7b3cbebc6fce4977e0033053a9
Marcel <marcel@nubiral.com>
2022-03-25 23:12:20 -0300

    x

--- bi-uy-testing/lnx.tf
+++ bi-uy-testing/lnx.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  assume_role {
-    role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_role_name}"
+  // assume_role {
+  //   role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_role_name}"
   }
 }
 resource "aws_instance" "web" {
