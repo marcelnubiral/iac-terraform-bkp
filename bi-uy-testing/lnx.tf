3f3aa922795883ea55aceed69c2b1ccad67504f3
Marcel <marcel@nubiral.com>
2022-03-25 23:29:51 -0300

    x

--- bi-uy-testing/lnx.tf
+++ bi-uy-testing/lnx.tf
@@ -5,7 +5,7 @@ provider "aws" {
   }
 
 resource "aws_instance" "web" {
-  ami           = "ami-089582bef738dbf6a"
+  ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
 
   tags = {
