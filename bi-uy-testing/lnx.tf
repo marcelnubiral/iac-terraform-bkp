6bf6771f513d3e9c79b37e102f4701cad6e02a3d
Marcel <marcel@nubiral.com>
2022-03-25 23:10:39 -0300

    x

--- bi-uy-testing/lnx.tf
+++ bi-uy-testing/lnx.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_role_name}"
+  }
+}
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
