6a602ef5ee1144f2e02030e706a60146d269bfc2
Marcel <marcel@nubiral.com>
2022-03-25 23:34:12 -0300

    x

--- bi-uy-testing/lnx.tf
+++ bi-uy-testing/lnx.tf
@@ -7,6 +7,8 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
+  security_groups = "sg-07a89f708579b7195"
+  subnet_id  = "subnet-0b53a6ae43e71d4b3"
 
   tags = {
     Name = "HelloWorld"
